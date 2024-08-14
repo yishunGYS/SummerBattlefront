4715fae12e91606aba6e3dc74e7389ba7ceea205
yishunGYS <18017288519@163.com>
2024-08-14 22:10:28 +0800

    增加排头兵死亡

--- Assets/Scripts/Gameplay/Player/Solider/HeadSoliderFeature/HeadSoliderLogic.cs
+++ Assets/Scripts/Gameplay/Player/Solider/HeadSoliderFeature/HeadSoliderLogic.cs
@@ -1,9 +1,20 @@
+using Managers;
+
 namespace Gameplay.Player
 {
     public class HeadSoliderLogic : SoliderLogicBase
     {
         public HeadSoliderLogic(SoliderAgent agent) : base(agent)
         {
+        }
+
+        protected override void Die()
+        {
+            base.Die();
+            if (BlockManager.instance.headSoliderBlocks.ContainsKey(soliderAgent))
+            {
+                BlockManager.instance.OnHeadSoliderDestory(soliderAgent);
+            }
         }
     }
 }
