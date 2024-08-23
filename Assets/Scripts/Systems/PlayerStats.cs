40622d42de6345b5309e4e04ed04de2fb4ee9eb6
yishunGYS <18017288519@163.com>
2024-08-23 20:01:10 +0800

    修复派兵UI没有耗能文字

--- Assets/Scripts/Systems/PlayerStats.cs
+++ Assets/Scripts/Systems/PlayerStats.cs
@@ -35,15 +35,13 @@ namespace Systems
 
         public void OnLevelStart()
         {
-
             // 获取关卡时间限制
             if (LevelManager.Instance != null)
             {
                 levelTimeLimit = LevelManager.Instance.GetCurrentLevelTime();
             }
-
             UIManager.Instance.OnOpenTimeLeftPanel();
-            
+            UIManager.Instance.OnOpenResourcePanel();
         }
 
         void FixedUpdate()
@@ -61,7 +59,6 @@ namespace Systems
         {
             remainingTime = levelTimeLimit;
             isLevelStarted = true;
-            UIManager.Instance.OnOpenResourcePanel();
             Debug.Log("关卡开始！");
         }
 
