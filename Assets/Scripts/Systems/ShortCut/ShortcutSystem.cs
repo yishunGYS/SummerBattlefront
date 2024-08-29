38e48a307d960de266512721d5c2debe088aacc8
yishunGYS <18017288519@163.com>
2024-08-29 16:10:11 +0800

    快捷键完成

---
+++ Assets/Scripts/Systems/ShortCut/ShortcutSystem.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Managers;
+using UI.Gameplay;
+using UnityEngine;
+using Utilities;
+
+namespace Systems.ShortCut
+{
+    public class ShortcutSystem : Singleton<ShortcutSystem>
+    {
+        private SpawnSoliderPanel spawnSoliderPanel;
+        private List<KeyCode> shortcuts = new List<KeyCode>(){KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8};
+        
+
+        private void Update()
+        {
+            if (spawnSoliderPanel == null)
+            {
+                spawnSoliderPanel = FindObjectOfType<SpawnSoliderPanel>();
+            }
+            
+            for (int i = 0; i < shortcuts.Count; i++)
+            {
+                if (Input.GetKeyDown(shortcuts[i]))
+                {
+                    var soliderInBattle = DataManager.Instance.GetSolidersInBattle();
+                    var soliderId = soliderInBattle[i];
+                    SpawnManager.Instance.ChangeSelectSolider(soliderId);
+                    spawnSoliderPanel.OnSelectCard(spawnSoliderPanel.soliderUILists[i].GetComponent<RectTransform>());
+                }
+            }
+
+        }
+    }
+}
